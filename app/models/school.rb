af0231fd2282b9213a178c118e691190e7d8a442
Prelang Builder <builder@prelang.com>
2015-07-01 12:43:05 +0000

    Defined validation for attribute 'school_url' on model 'School'

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,4 +2,5 @@ class School < ActiveRecord::Base
   has_many :users
   has_many :reviews
   validates_formatting_of :school_email, using: :email
+  validates_formatting_of :school_url, using: :url
 end
